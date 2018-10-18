8b2fe406261b3a990fbec57af3ea3fced7aefc59
gorniak <penalizacja@gmail.com>
2018-10-18 21:46:02 +0200

    Initial final project commit.

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,17 @@
-// Global app controller
+const key = '5d73cad3321edf16595b9aff5b9c8dd4'
+//https://www.food2fork.com/api/search
+
+import axios from 'axios';
+import { get } from 'http';
+
+
+async function getResults(query) {
+   try {
+        const result = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = result.data.recipes
+        console.log (recipes);
+   }catch (error) {
+       alert(error);
+   }
+}
+getResults('tomato pasta');
